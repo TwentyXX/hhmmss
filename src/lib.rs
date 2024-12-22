c2619f5a64fc8405eb05e79ad37ac72937011b8d
TwentyXX <twenty.takumi1024@gmail.com>
2024-12-22 19:24:26 +0900

    Overall functionality added

--- src/lib.rs
+++ src/lib.rs
@@ -1,224 +1,246 @@
 pub trait Hhmmss {
-	fn s_ms(&self) -> (i64, i64);
-	/// Pretty-prints a chrono::Duration in the form `HH:MM:SS.xxx`
-	fn hhmmss(&self) -> String {
-		let (s, _ms) = self.s_ms();
-		s2hhmmss(s)
+	fn sign(&self) -> String {
+		if self.is_negative() {
+			"-".to_owned()
+		} else {
+			"".to_owned()
+		}
 	}
+	fn is_negative(&self) -> bool;
+	fn unsigned_hours(&self) -> u64;
+	fn unsigned_minutes(&self) -> u64;
+	fn unsigned_seconds(&self) -> u64;
+	fn unsigned_milliseconds(&self) -> u64;
+	fn unsigned_microseconds(&self) -> u64;
+	fn unsigned_nanoseconds(&self) -> u64;
+	fn get_hours(&self) -> i64;
+	fn get_minutes(&self) -> i64;
+	fn get_seconds(&self) -> i64;
+	fn get_milliseconds(&self) -> i64;
+	fn get_microseconds(&self) -> i64;
+	fn get_nanoseconds(&self) -> i64;
 
-	#[test]
-	fn test_hmmss() {
-		let std_duration = std::time::Duration::new((1 * 60 + 23) * 60 + 45, 0);
-		assert_eq!(&std_duration.hmmss(), "1:23:45");
-		let chrono_duration = chrono::Duration::from_std(std_duration).unwrap();
-		assert_eq!(&chrono_duration.hmmss(), "1:23:45");
-		let time_duration = time::Duration::from_std(std_duration).unwrap();
-		assert_eq!(&time_duration.hmmss(), "1:23:45");
-	}
+	fn unsigned_hh(&self) -> String { format!("{:02}", self.unsigned_hours()) }
+	fn unsigned_mm(&self) -> String { format!("{:02}", self.unsigned_minutes()) }
+	fn unsigned_ss(&self) -> String { format!("{:02}", self.unsigned_seconds()) }
+	fn unsigned_xxx(&self) -> String { format!("{:03}", self.unsigned_milliseconds()) }
 
-	#[test]
-	fn test_hmmssxxx() {
-		let std_duration = std::time::Duration::new((1 * 60 + 23) * 60 + 45, 678_901_234);
-		assert_eq!(&std_duration.hmmssxxx(), "1:23:45.678");
-		let chrono_duration = chrono::Duration::from_std(std_duration).unwrap();
-		assert_eq!(&chrono_duration.hmmssxxx(), "1:23:45.678");
-		let time_duration = time::Duration::from_std(std_duration).unwrap();
-		assert_eq!(&time_duration.hmmssxxx(), "1:23:45.678");
-	}
+	fn fmt_hh(&self) -> String { self.sign() + &self.unsigned_hh() }
+	fn fmt_mm(&self) -> String { self.sign() + &self.unsigned_mm() }
+	fn fmt_ss(&self) -> String { self.sign() + &self.unsigned_ss() }
+	fn fmt_xxx(&self) -> String { self.sign() + &self.unsigned_xxx() }
 
-	#[test]
-	fn test_mss() {
-		let std_duration = std::time::Duration::new((1 * 60 + 23) * 60 + 45, 0);
-		assert_eq!(&std_duration.mss(), "23:45");
-		let chrono_duration = chrono::Duration::from_std(std_duration).unwrap();
-		assert_eq!(&chrono_duration.mss(), "23:45");
-		let time_duration = time::Duration::from_std(std_duration).unwrap();
-		assert_eq!(&time_duration.mss(), "23:45");
-	}
-
-	#[test]
-	fn test_mssxxx() {
-		let std_duration = std::time::Duration::new((1 * 60 + 23) * 60 + 45, 678_901_234);
-		assert_eq!(&std_duration.mssxxx(), "23:45.678");
-		let chrono_duration = chrono::Duration::from_std(std_duration).unwrap();
-		assert_eq!(&chrono_duration.mssxxx(), "23:45.678");
-		let time_duration = time::Duration::from_std(std_duration).unwrap();
-		assert_eq!(&time_duration.mssxxx(), "23:45.678");
-	}
-	/// Pretty-prints a chrono::Duration in the form `HH:MM:SS.xxx`
-	fn hhmmssxxx(&self) -> String {
-		let (s, ms) = self.s_ms();
-		format_duration_hhmmssxxx(s, ms)
-	}
-	/// Pretty-print chrono::Duration in the format
-	/// `H:MM:SS` if the hour value is one digit,
-	/// otherwise in the format `HH:MM:SS`.
-	fn hmmss(&self) -> String {
-		let (s, _ms) = self.s_ms();
-		s2hhmmss(s)
-	}
-	/// Pretty-print chrono::Duration in the format
-	/// `H:MM:SS.xxx` if the hour value is one digit,
-	/// otherwise in the format `HH:MM:SS.xxx`.
-	fn hmmssxxx(&self) -> String {
-		let (s, ms) = self.s_ms();
-		sms2hhmmsxxx(s, ms)
-	}
 	/// Pretty-prints a chrono::Duration in the form `MM:SS`
-	fn mmss(&self) -> String {
-		let (s, _ms) = self.s_ms();
-		s2mmss(s)
-	}
+	fn unsigned_mmss(&self) -> String { format!("{}:{}", self.unsigned_mm(), self.unsigned_ss()) }
 	/// Pretty-prints a chrono::Duration in the form `MM:SS.xxx`
-	fn mmssxxx(&self) -> String {
-		let (s, ms) = self.s_ms();
-		format_duration_mmssxxx(s, ms)
+	fn unsigned_mmssxxx(&self) -> String {
+		format!(
+			"{}:{}.{}",
+			self.unsigned_hh(),
+			self.unsigned_ss(),
+			self.unsigned_xxx()
+		)
 	}
 	/// Pretty-print chrono::Duration in the format
 	/// `M:SS` if the minute value is one digit,
 	/// otherwise in the format `MM:SS`.
-	fn mss(&self) -> String {
-		let (s, _ms) = self.s_ms();
-		s2mmss(s)
+	fn unsigned_mss(&self) -> String {
+		format!("{}:{}", self.unsigned_minutes(), self.unsigned_ss())
 	}
 	/// Pretty-print chrono::Duration in the format
 	/// `M:SS.xxx` if the minute value is one digit,
 	/// otherwise in the format `MM:SS.xxx`.
-	fn mssxxx(&self) -> String {
-		let (s, ms) = self.s_ms();
-		sms2mmsxxx(s, ms)
+	fn unsigned_mssxxx(&self) -> String {
+		format!(
+			"{}:{}.{}",
+			self.unsigned_minutes(),
+			self.unsigned_ss(),
+			self.unsigned_xxx(),
+		)
 	}
+	/// Pretty-prints a chrono::Duration in the form `HH:MM:SS.xxx`
+	fn unsigned_hhmmss(&self) -> String {
+		format!(
+			"{}:{}:{}",
+			self.unsigned_hh(),
+			self.unsigned_mm(),
+			self.unsigned_ss(),
+		)
+	}
+
+	/// Pretty-prints a chrono::Duration in the form `HH:MM:SS.xxx`
+	fn unsigned_hhmmssxxx(&self) -> String {
+		format!(
+			"{}:{}:{}.{}",
+			self.unsigned_hh(),
+			self.unsigned_mm(),
+			self.unsigned_ss(),
+			self.unsigned_xxx()
+		)
+	}
+	fn unsigned_hmmss(&self) -> String {
+		format!(
+			"{}:{}:{}",
+			self.unsigned_hours(),
+			self.unsigned_mm(),
+			self.unsigned_ss()
+		)
+	}
+	fn unsigned_hmmssxxx(&self) -> String {
+		format!(
+			"{}:{}:{}.{}",
+			self.unsigned_hours(),
+			self.unsigned_mm(),
+			self.unsigned_ss(),
+			self.unsigned_xxx()
+		)
+	}
+	fn mmss(&self) -> String { self.sign() + &self.unsigned_mmss() }
+	fn mmssxxx(&self) -> String { self.sign() + &self.unsigned_mmssxxx() }
+	fn mss(&self) -> String { self.sign() + &self.unsigned_mss() }
+	fn mssxxx(&self) -> String { self.sign() + &self.unsigned_mssxxx() }
+	fn hhmmss(&self) -> String { self.sign() + &self.unsigned_hhmmss() }
+	fn hhmmssxxx(&self) -> String { self.sign() + &self.unsigned_hhmmssxxx() }
+	fn hmmss(&self) -> String { self.sign() + &self.unsigned_hmmss() }
+	fn hmmssxxx(&self) -> String { self.sign() + &self.unsigned_hmmssxxx() }
 }
 
 impl Hhmmss for chrono::Duration {
-	fn s_ms(&self) -> (i64, i64) {
-		let s = self.num_seconds();
-		let ms = self.num_milliseconds() - 1000 * s;
-		(s, ms)
+	fn get_hours(&self) -> i64 { self.num_hours() }
+
+	fn get_minutes(&self) -> i64 { self.num_minutes() }
+
+	fn get_seconds(&self) -> i64 { self.num_seconds() }
+
+	fn get_milliseconds(&self) -> i64 { self.num_milliseconds() }
+
+	fn get_microseconds(&self) -> i64 { self.num_microseconds().unwrap_or(0) }
+
+	fn get_nanoseconds(&self) -> i64 { self.num_nanoseconds().unwrap_or(0) }
+
+	fn unsigned_hours(&self) -> u64 { self.num_hours().unsigned_abs() }
+
+	fn unsigned_minutes(&self) -> u64 { self.num_minutes().unsigned_abs() % 60 }
+
+	fn unsigned_seconds(&self) -> u64 { self.num_seconds().unsigned_abs() % 60 }
+
+	fn unsigned_milliseconds(&self) -> u64 { self.num_milliseconds().unsigned_abs() % 1000 }
+
+	fn unsigned_microseconds(&self) -> u64 {
+		self.num_microseconds().unwrap_or(0).unsigned_abs() % 1000
 	}
+
+	fn unsigned_nanoseconds(&self) -> u64 {
+		self.num_nanoseconds().unwrap_or(0).unsigned_abs() % 1000
+	}
+
+	fn is_negative(&self) -> bool { self.num_seconds() < 0 }
 }
 
 impl Hhmmss for std::time::Duration {
-	fn s_ms(&self) -> (i64, i64) {
-		let s = self.as_secs();
-		let ms = self.subsec_millis();
-		(s as i64, ms as i64)
-	}
+	fn get_hours(&self) -> i64 { self.as_secs() as i64 / 3600 }
+
+	fn get_minutes(&self) -> i64 { (self.as_secs() as i64 / 60) % 60 }
+
+	fn get_seconds(&self) -> i64 { self.as_secs() as i64 % 60 }
+
+	fn get_milliseconds(&self) -> i64 { self.subsec_millis() as i64 }
+
+	fn get_microseconds(&self) -> i64 { self.subsec_micros() as i64 }
+
+	fn get_nanoseconds(&self) -> i64 { self.subsec_nanos() as i64 }
+
+	fn unsigned_hours(&self) -> u64 { self.as_secs() / 3600 }
+
+	fn unsigned_minutes(&self) -> u64 { (self.as_secs() / 60) % 60 }
+
+	fn unsigned_seconds(&self) -> u64 { self.as_secs() % 60 }
+
+	fn unsigned_milliseconds(&self) -> u64 { self.subsec_millis() as u64 }
+
+	fn unsigned_microseconds(&self) -> u64 { self.subsec_micros() as u64 }
+
+	fn unsigned_nanoseconds(&self) -> u64 { self.subsec_nanos() as u64 }
+
+	fn is_negative(&self) -> bool { false }
+
+	fn sign(&self) -> String { "".to_owned() }
 }
 
 impl Hhmmss for time::Duration {
-	fn s_ms(&self) -> (i64, i64) {
-		let s = self.whole_seconds();
-		let ms = self.whole_milliseconds() as i64 - 1000 * s;
-		(s, ms)
+	fn get_hours(&self) -> i64 { self.whole_hours() }
+
+	fn get_minutes(&self) -> i64 { self.whole_minutes() }
+
+	fn get_seconds(&self) -> i64 { self.whole_seconds() }
+
+	fn get_milliseconds(&self) -> i64 { self.subsec_milliseconds() as i64 }
+
+	fn get_microseconds(&self) -> i64 { self.subsec_microseconds() as i64 }
+
+	fn get_nanoseconds(&self) -> i64 { self.subsec_nanoseconds() as i64 }
+
+	fn unsigned_hours(&self) -> u64 { (self.whole_hours().unsigned_abs()) }
+
+	fn unsigned_minutes(&self) -> u64 { (self.whole_minutes().unsigned_abs() % 60) }
+
+	fn unsigned_seconds(&self) -> u64 { (self.whole_seconds().unsigned_abs() % 60) }
+
+	fn unsigned_milliseconds(&self) -> u64 {
+		(self.subsec_milliseconds().unsigned_abs() as u64 % 1000)
 	}
-}
 
-fn format_duration_hhmmssxxx(s: i64, ms: i64) -> String {
-	let (neg, h, m, s, ms) = s2tuple(s, ms);
-	format!(
-		"{}{}:{:02}:{:02}.{:03}",
-		if neg { "-" } else { "" },
-		if h < 10 { format!("{}", h) } else { format!("{:02}", h) },
-		m,
-		s,
-		ms
-	)
-}
-
-fn format_duration_mmssxxx(s: i64, ms: i64) -> String {
-	let (neg, _h, m, s, ms) = s2tuple(s, ms);
-	format!(
-		"{}{}:{:02}.{:03}",
-		if neg { "-" } else { "" },
-		if m < 10 { format!("{}", m) } else { format!("{:02}", m) },
-		s,
-		ms
-	)
-}
-
-fn s2hhmmss(s: i64) -> String {
-	let mut neg = false;
-	let mut s = s;
-	if s < 0 {
-		neg = true;
-		s = -s;
+	fn unsigned_microseconds(&self) -> u64 {
+		(self.subsec_microseconds().unsigned_abs() as u64 % 1000)
 	}
-	let (h, s) = (s / 3600, s % 3600);
-	let (m, s) = (s / 60, s % 60);
-	format!("{}{:02}:{:02}:{:02}", if neg { "-" } else { "" }, h, m, s)
-}
 
-fn sms2hhmmsxxx(s: i64, ms: i64) -> String {
-	let mut neg = false;
-	let (mut s, mut ms) = (s, ms);
-	if s < 0 {
-		neg = true;
-		s = -s;
-		ms = -ms;
+	fn unsigned_nanoseconds(&self) -> u64 {
+		(self.subsec_nanoseconds().unsigned_abs() as u64 % 1000)
 	}
-	let (h, s) = (s / 3600, s % 3600);
-	let (m, s) = (s / 60, s % 60);
-	format!(
-		"{}{:02}:{:02}:{:02}.{:03}",
-		if neg { "-" } else { "" },
-		h,
-		m,
-		s,
-		ms
-	)
+
+	fn is_negative(&self) -> bool { self.whole_seconds() < 0 }
 }
 
-fn s2mmss(s: i64) -> String {
-	let mut neg = false;
-	let mut s = s;
-	if s < 0 {
-		neg = true;
-		s = -s;
-	}
-	let (_h, s) = (s / 3600, s % 3600);
-	let (m, s) = (s / 60, s % 60);
-	format!("{}{:02}:{:02}", if neg { "-" } else { "" }, m, s)
-}
-
-fn sms2mmsxxx(s: i64, ms: i64) -> String {
-	let (neg, _h, m, s, ms) = s2tuple(s, ms);
-	format!("{}{:02}:{:02}.{:03}", if neg { "-" } else { "" }, m, s, ms)
-}
-
-fn s2tuple(s: i64, ms: i64) -> (bool, i64, i64, i64, i64) {
-	let mut neg = false;
-	let (mut s, mut ms) = (s, ms);
-	if s < 0 {
-		neg = true;
-		s = -s;
-		ms = -ms;
-	}
-	let (h, s) = (s / 3600, s % 3600);
-	let (m, s) = (s / 60, s % 60);
-	(neg, h, m, s, ms)
-}
 #[cfg(test)]
 mod tests {
 
 	use super::*;
 
 	#[test]
-	fn test_all() {
+	fn test_all_crate_durations() {
 		let std_duration = std::time::Duration::new((1 * 60 + 23) * 60 + 45, 678_901_234);
-		assert_eq!(&std_duration.hhmmss(), "01:23:45");
-		assert_eq!(&std_duration.hhmmssxxx(), "01:23:45.678");
-		assert_eq!(&std_duration.mmss(), "23:45");
-		assert_eq!(&std_duration.mmssxxx(), "23:45.678");
+		assert_eq!(&std_duration.unsigned_hhmmss(), "01:23:45");
+		assert_eq!(&std_duration.unsigned_hhmmssxxx(), "01:23:45.678");
+		assert_eq!(&std_duration.unsigned_mmss(), "23:45");
+		assert_eq!(&std_duration.unsigned_mmssxxx(), "23:45.678");
+		assert_eq!(&std_duration.unsigned_hmmss(), "1:23:45");
+		assert_eq!(&std_duration.unsigned_hmmssxxx(), "1:23:45.678");
 		let chrono_duration = chrono::Duration::from_std(std_duration).unwrap();
-		assert_eq!(&chrono_duration.hhmmss(), "01:23:45");
-		assert_eq!(&chrono_duration.hhmmssxxx(), "01:23:45.678");
-		assert_eq!(&chrono_duration.mmss(), "23:45");
-		assert_eq!(&chrono_duration.mmssxxx(), "23:45.678");
+		assert_eq!(&chrono_duration.unsigned_hhmmssxxx(), "01:23:45.678");
 		let time_duration = time::Duration::from_std(std_duration).unwrap();
-		assert_eq!(&time_duration.hhmmss(), "01:23:45");
-		assert_eq!(&time_duration.hhmmssxxx(), "01:23:45.678");
-		assert_eq!(&time_duration.mmss(), "23:45");
-		assert_eq!(&time_duration.mmssxxx(), "23:45.678");
+		assert_eq!(&time_duration.unsigned_hhmmssxxx(), "01:23:45.678");
+	}
+
+	#[test]
+	fn test_negative_durations() {
+		let minus_one_hour = chrono::Duration::hours(-1);
+		dbg!(minus_one_hour);
+		assert_eq!(&minus_one_hour.hmmss(), "-1:00:00");
+		assert_eq!(&minus_one_hour.hhmmss(), "-01:00:00");
+		assert_eq!(minus_one_hour.unsigned_hours(), 1);
+		assert_eq!(minus_one_hour.get_hours(), -1);
+		assert_eq!(&minus_one_hour.unsigned_hh(), "01");
+		assert_eq!(&minus_one_hour.unsigned_hmmss(), "1:00:00");
+	}
+
+	#[test]
+	fn debug_print() {
+		let std_duration = std::time::Duration::new((1 * 60 + 23) * 60 + 45, 678_901_234);
+		dbg!(&std_duration.unsigned_hhmmss());
+		dbg!(&std_duration.unsigned_hhmmssxxx());
+		dbg!(&std_duration.unsigned_mmss());
+		dbg!(&std_duration.unsigned_mmssxxx());
+		dbg!(&std_duration.unsigned_hmmss());
+		dbg!(&std_duration.unsigned_hmmssxxx());
 	}
 }
