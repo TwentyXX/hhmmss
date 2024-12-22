0dd7655c1ba10aa613f0bbe85299433866a0afa7
TwentyXX <twenty.takumi1024@gmail.com>
2024-12-22 20:54:51 +0900

    formatted

--- src/lib.rs
+++ src/lib.rs
@@ -219,26 +219,26 @@ impl Hhmmss for time::Duration {
 		(self.subsec_nanoseconds() % Self::NANOSECONDS_IN_A_MICROSECOND as i32) as i64
 	}
 
-	fn part_of_hours_abs(&self) -> u64 { (self.whole_hours().unsigned_abs()) }
+	fn part_of_hours_abs(&self) -> u64 { self.whole_hours().unsigned_abs() }
 
 	fn part_of_minutes_abs(&self) -> u64 {
-		(self.whole_minutes().unsigned_abs() % Self::MINUTES_IN_AN_HOUR)
+		self.whole_minutes().unsigned_abs() % Self::MINUTES_IN_AN_HOUR
 	}
 
 	fn part_of_seconds_abs(&self) -> u64 {
-		(self.whole_seconds().unsigned_abs() % Self::SECONDS_IN_A_MINUTE)
+		self.whole_seconds().unsigned_abs() % Self::SECONDS_IN_A_MINUTE
 	}
 
 	fn part_of_milliseconds_abs(&self) -> u64 {
-		(self.subsec_milliseconds().unsigned_abs() as u64 % Self::MILLISECONDS_IN_A_SECOND)
+		self.subsec_milliseconds().unsigned_abs() as u64 % Self::MILLISECONDS_IN_A_SECOND
 	}
 
 	fn part_of_microseconds_abs(&self) -> u64 {
-		(self.subsec_microseconds().unsigned_abs() as u64 % Self::MICROSECONDS_IN_A_MILLISECOND)
+		self.subsec_microseconds().unsigned_abs() as u64 % Self::MICROSECONDS_IN_A_MILLISECOND
 	}
 
 	fn part_of_nanoseconds_abs(&self) -> u64 {
-		(self.subsec_nanoseconds().unsigned_abs() as u64 % Self::NANOSECONDS_IN_A_MICROSECOND)
+		self.subsec_nanoseconds().unsigned_abs() as u64 % Self::NANOSECONDS_IN_A_MICROSECOND
 	}
 
 	fn is_negative(&self) -> bool { self.whole_seconds() < 0 }
@@ -247,7 +247,7 @@ impl Hhmmss for time::Duration {
 #[cfg(test)]
 mod tests {
 
-	use super::*;
+	use crate::Hhmmss;
 
 	#[test]
 	fn test_all_crate_durations() {
@@ -260,7 +260,7 @@ mod tests {
 		assert_eq!(&std_duration.unsigned_hmmssxxx(), "1:23:45.678");
 		let chrono_duration = chrono::Duration::from_std(std_duration).unwrap();
 		assert_eq!(&chrono_duration.unsigned_hhmmssxxx(), "01:23:45.678");
-		let time_duration = time::Duration::from_std(std_duration).unwrap();
+		let time_duration = time::Duration::try_from(std_duration).unwrap();
 		assert_eq!(&time_duration.unsigned_hhmmssxxx(), "01:23:45.678");
 	}
 
@@ -268,87 +268,98 @@ mod tests {
 	fn test_all_features() {
 		let minus_one_hour = chrono::Duration::seconds(-((1 * 60 + 23) * 60 + 45))
 			+ chrono::Duration::nanoseconds(-678_901_234);
+		test(minus_one_hour);
+		let minus_one_hour = time::Duration::new(-((1 * 60 + 23) * 60 + 45), -678_901_234);
+		test(minus_one_hour);
 
-		assert_eq!(minus_one_hour.part_of_hours(), -1);
-		assert_eq!(minus_one_hour.part_of_minutes(), -23);
-		assert_eq!(minus_one_hour.part_of_seconds(), -45);
-		assert_eq!(minus_one_hour.part_of_milliseconds(), -678);
-		assert_eq!(minus_one_hour.part_of_microseconds(), -901);
-		assert_eq!(minus_one_hour.part_of_nanoseconds(), -234);
-		assert_eq!(minus_one_hour.part_of_hours_abs(), 1);
-		assert_eq!(minus_one_hour.part_of_minutes_abs(), 23);
-		assert_eq!(minus_one_hour.part_of_seconds_abs(), 45);
-		assert_eq!(minus_one_hour.part_of_milliseconds_abs(), 678);
-		assert_eq!(minus_one_hour.part_of_microseconds_abs(), 901);
-		assert_eq!(minus_one_hour.part_of_nanoseconds_abs(), 234);
-		assert_eq!(minus_one_hour.is_negative(), true);
+		fn test<T: Hhmmss>(minus_one_hour: T) {
+			assert_eq!(minus_one_hour.part_of_hours(), -1);
+			assert_eq!(minus_one_hour.part_of_minutes(), -23);
+			assert_eq!(minus_one_hour.part_of_seconds(), -45);
+			assert_eq!(minus_one_hour.part_of_milliseconds(), -678);
+			assert_eq!(minus_one_hour.part_of_microseconds(), -901);
+			assert_eq!(minus_one_hour.part_of_nanoseconds(), -234);
+			assert_eq!(minus_one_hour.part_of_hours_abs(), 1);
+			assert_eq!(minus_one_hour.part_of_minutes_abs(), 23);
+			assert_eq!(minus_one_hour.part_of_seconds_abs(), 45);
+			assert_eq!(minus_one_hour.part_of_milliseconds_abs(), 678);
+			assert_eq!(minus_one_hour.part_of_microseconds_abs(), 901);
+			assert_eq!(minus_one_hour.part_of_nanoseconds_abs(), 234);
+			assert_eq!(minus_one_hour.is_negative(), true);
 
-		assert_eq!(&minus_one_hour.fmt_hh(), "-01");
-		assert_eq!(&minus_one_hour.fmt_mm(), "-23");
-		assert_eq!(&minus_one_hour.fmt_ss(), "-45");
-		assert_eq!(&minus_one_hour.mss(), "-23:45");
-		assert_eq!(&minus_one_hour.mmss(), "-23:45");
-		assert_eq!(&minus_one_hour.hmmss(), "-1:23:45");
-		assert_eq!(&minus_one_hour.hhmmss(), "-01:23:45");
-		assert_eq!(&minus_one_hour.mssxxx(), "-23:45.678");
-		assert_eq!(&minus_one_hour.mmssxxx(), "-23:45.678");
-		assert_eq!(&minus_one_hour.hmmssxxx(), "-1:23:45.678");
-		assert_eq!(&minus_one_hour.hhmmssxxx(), "-01:23:45.678");
-		assert_eq!(&minus_one_hour.unsigned_hh(), "01");
-		assert_eq!(&minus_one_hour.unsigned_mm(), "23");
-		assert_eq!(&minus_one_hour.unsigned_ss(), "45");
-		assert_eq!(&minus_one_hour.unsigned_mss(), "23:45");
-		assert_eq!(&minus_one_hour.unsigned_mmss(), "23:45");
-		assert_eq!(&minus_one_hour.unsigned_hmmss(), "1:23:45");
-		assert_eq!(&minus_one_hour.unsigned_hhmmss(), "01:23:45");
-		assert_eq!(&minus_one_hour.unsigned_mssxxx(), "23:45.678");
-		assert_eq!(&minus_one_hour.unsigned_mmssxxx(), "23:45.678");
-		assert_eq!(&minus_one_hour.unsigned_hmmssxxx(), "1:23:45.678");
-		assert_eq!(&minus_one_hour.unsigned_hhmmssxxx(), "01:23:45.678");
+			assert_eq!(&minus_one_hour.fmt_hh(), "-01");
+			assert_eq!(&minus_one_hour.fmt_mm(), "-23");
+			assert_eq!(&minus_one_hour.fmt_ss(), "-45");
+			assert_eq!(&minus_one_hour.mss(), "-23:45");
+			assert_eq!(&minus_one_hour.mmss(), "-23:45");
+			assert_eq!(&minus_one_hour.hmmss(), "-1:23:45");
+			assert_eq!(&minus_one_hour.hhmmss(), "-01:23:45");
+			assert_eq!(&minus_one_hour.mssxxx(), "-23:45.678");
+			assert_eq!(&minus_one_hour.mmssxxx(), "-23:45.678");
+			assert_eq!(&minus_one_hour.hmmssxxx(), "-1:23:45.678");
+			assert_eq!(&minus_one_hour.hhmmssxxx(), "-01:23:45.678");
+			assert_eq!(&minus_one_hour.unsigned_hh(), "01");
+			assert_eq!(&minus_one_hour.unsigned_mm(), "23");
+			assert_eq!(&minus_one_hour.unsigned_ss(), "45");
+			assert_eq!(&minus_one_hour.unsigned_mss(), "23:45");
+			assert_eq!(&minus_one_hour.unsigned_mmss(), "23:45");
+			assert_eq!(&minus_one_hour.unsigned_hmmss(), "1:23:45");
+			assert_eq!(&minus_one_hour.unsigned_hhmmss(), "01:23:45");
+			assert_eq!(&minus_one_hour.unsigned_mssxxx(), "23:45.678");
+			assert_eq!(&minus_one_hour.unsigned_mmssxxx(), "23:45.678");
+			assert_eq!(&minus_one_hour.unsigned_hmmssxxx(), "1:23:45.678");
+			assert_eq!(&minus_one_hour.unsigned_hhmmssxxx(), "01:23:45.678");
 
-		assert_eq!(&minus_one_hour.get_sign(), "-");
+			assert_eq!(&minus_one_hour.get_sign(), "-");
+		}
 	}
 	#[test]
 	fn test_all_funcs_without_negative() {
-		let sample_val = std::time::Duration::new(((1 * 60 + 23) * 60 + 45), 678_901_234);
-		let sample_val = time::Duration::from_std(sample_val).unwrap();
+		let std_duration = std::time::Duration::new((1 * 60 + 23) * 60 + 45, 678_901_234);
+		test(std_duration);
+		let sample_val = time::Duration::try_from(std_duration).unwrap();
+		test(sample_val);
+		let sample_val = chrono::Duration::from_std(std_duration).unwrap();
+		test(sample_val);
 
-		assert_eq!(sample_val.part_of_hours(), 1);
-		assert_eq!(sample_val.part_of_minutes(), 23);
-		assert_eq!(sample_val.part_of_seconds(), 45);
-		assert_eq!(sample_val.part_of_milliseconds(), 678);
-		assert_eq!(sample_val.part_of_microseconds(), 901);
-		assert_eq!(sample_val.part_of_nanoseconds(), 234);
-		assert_eq!(sample_val.part_of_hours_abs(), 1);
-		assert_eq!(sample_val.part_of_minutes_abs(), 23);
-		assert_eq!(sample_val.part_of_seconds_abs(), 45);
-		assert_eq!(sample_val.part_of_milliseconds_abs(), 678);
-		assert_eq!(sample_val.part_of_microseconds_abs(), 901);
-		assert_eq!(sample_val.part_of_nanoseconds_abs(), 234);
+		fn test<T: Hhmmss>(sample_val: T) {
+			assert_eq!(sample_val.part_of_hours(), 1);
+			assert_eq!(sample_val.part_of_minutes(), 23);
+			assert_eq!(sample_val.part_of_seconds(), 45);
+			assert_eq!(sample_val.part_of_milliseconds(), 678);
+			assert_eq!(sample_val.part_of_microseconds(), 901);
+			assert_eq!(sample_val.part_of_nanoseconds(), 234);
+			assert_eq!(sample_val.part_of_hours_abs(), 1);
+			assert_eq!(sample_val.part_of_minutes_abs(), 23);
+			assert_eq!(sample_val.part_of_seconds_abs(), 45);
+			assert_eq!(sample_val.part_of_milliseconds_abs(), 678);
+			assert_eq!(sample_val.part_of_microseconds_abs(), 901);
+			assert_eq!(sample_val.part_of_nanoseconds_abs(), 234);
 
-		assert_eq!(&sample_val.fmt_hh(), "01");
-		assert_eq!(&sample_val.fmt_mm(), "23");
-		assert_eq!(&sample_val.fmt_ss(), "45");
-		assert_eq!(&sample_val.mss(), "23:45");
-		assert_eq!(&sample_val.mmss(), "23:45");
-		assert_eq!(&sample_val.hmmss(), "1:23:45");
-		assert_eq!(&sample_val.hhmmss(), "01:23:45");
-		assert_eq!(&sample_val.mssxxx(), "23:45.678");
-		assert_eq!(&sample_val.mmssxxx(), "23:45.678");
-		assert_eq!(&sample_val.hmmssxxx(), "1:23:45.678");
-		assert_eq!(&sample_val.hhmmssxxx(), "01:23:45.678");
-		assert_eq!(&sample_val.unsigned_hh(), "01");
-		assert_eq!(&sample_val.unsigned_mm(), "23");
-		assert_eq!(&sample_val.unsigned_ss(), "45");
-		assert_eq!(&sample_val.unsigned_mss(), "23:45");
-		assert_eq!(&sample_val.unsigned_mmss(), "23:45");
-		assert_eq!(&sample_val.unsigned_hmmss(), "1:23:45");
-		assert_eq!(&sample_val.unsigned_hhmmss(), "01:23:45");
-		assert_eq!(&sample_val.unsigned_mssxxx(), "23:45.678");
-		assert_eq!(&sample_val.unsigned_mmssxxx(), "23:45.678");
-		assert_eq!(&sample_val.unsigned_hmmssxxx(), "1:23:45.678");
-		assert_eq!(&sample_val.unsigned_hhmmssxxx(), "01:23:45.678");
+			assert_eq!(&sample_val.fmt_hh(), "01");
+			assert_eq!(&sample_val.fmt_mm(), "23");
+			assert_eq!(&sample_val.fmt_ss(), "45");
+			assert_eq!(&sample_val.mss(), "23:45");
+			assert_eq!(&sample_val.mmss(), "23:45");
+			assert_eq!(&sample_val.hmmss(), "1:23:45");
+			assert_eq!(&sample_val.hhmmss(), "01:23:45");
+			assert_eq!(&sample_val.mssxxx(), "23:45.678");
+			assert_eq!(&sample_val.mmssxxx(), "23:45.678");
+			assert_eq!(&sample_val.hmmssxxx(), "1:23:45.678");
+			assert_eq!(&sample_val.hhmmssxxx(), "01:23:45.678");
+			assert_eq!(&sample_val.unsigned_hh(), "01");
+			assert_eq!(&sample_val.unsigned_mm(), "23");
+			assert_eq!(&sample_val.unsigned_ss(), "45");
+			assert_eq!(&sample_val.unsigned_mss(), "23:45");
+			assert_eq!(&sample_val.unsigned_mmss(), "23:45");
+			assert_eq!(&sample_val.unsigned_hmmss(), "1:23:45");
+			assert_eq!(&sample_val.unsigned_hhmmss(), "01:23:45");
+			assert_eq!(&sample_val.unsigned_mssxxx(), "23:45.678");
+			assert_eq!(&sample_val.unsigned_mmssxxx(), "23:45.678");
+			assert_eq!(&sample_val.unsigned_hmmssxxx(), "1:23:45.678");
+			assert_eq!(&sample_val.unsigned_hhmmssxxx(), "01:23:45.678");
 
-		assert_eq!(&sample_val.get_sign(), "");
+			assert_eq!(&sample_val.get_sign(), "");
+		}
 	}
 }
