8a4b65b11607e993c058e5bfa677a83b53748ad7
TwentyXX <twenty.takumi1024@gmail.com>
2024-12-29 12:47:11 +0900

    added implement untis_per_sec

--- src/fract.rs
+++ src/fract.rs
@@ -3,3 +3,13 @@ pub enum FractPartOfDuration {
 	Microseconds,
 	Nanoseconds,
 }
+
+impl FractPartOfDuration {
+	pub fn units_per_sec(&self) -> u32 {
+		match self {
+			FractPartOfDuration::Milliseconds => 1_000,
+			FractPartOfDuration::Microseconds => 1_000_000,
+			FractPartOfDuration::Nanoseconds => 1_000_000_000,
+		}
+	}
+}
